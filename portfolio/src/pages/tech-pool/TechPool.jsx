916a116c16eac5dd2a25a2a384280666557a8d3b
gabrielNardone <gabriel.f.nardone@gmail.com>
2023-10-18 09:45:35 -0300

    changes

--- portfolio/src/pages/tech-pool/TechPool.jsx
+++ portfolio/src/pages/tech-pool/TechPool.jsx
@@ -13,21 +13,26 @@ export const TechPool = () => {
           <span><i className="fa-solid fa-fish"></i></span>
           <span>TypeScript</span>
           <span><i className="fa-solid fa-fish"></i></span>
+          <span>React Native</span>
+          <span><i className="fa-solid fa-fish"></i></span>
           <span>HTML</span>
           <span><i className="fa-solid fa-fish"></i></span>
           <span>CSS</span>
           <span><i className="fa-solid fa-fish"></i></span>
-          <span>Boostrap</span>
+          <span>Material UI</span>
           <span><i className="fa-solid fa-fish"></i></span>
           <span>TailWind</span>
           <span><i className="fa-solid fa-fish"></i></span>
           <span>Git</span>
+          <span><i className="fa-solid fa-fish"></i></span>
         </div>
       </article>
       <article className="secondArticle">
         <h2 className="redH2">Acquired skills:</h2>
         <div>
           <span>Java</span>
+          <i className="fa-solid fa-seedling"></i>
+          <span>Maven</span>
           <i className="fa-solid fa-seedling"></i>
           <span>Spring</span>
           <i className="fa-solid fa-seedling"></i>
@@ -38,34 +43,35 @@ export const TechPool = () => {
           <span>Postman</span>
           <i className="fa-solid fa-seedling"></i>
           <span>AWS Cloud Formation</span>
+          <i className="fa-solid fa-seedling"></i>
+          <span>Google Firebase</span>
+          <i className="fa-solid fa-seedling"></i>
         </div>
       </article>
       <article className="thirdArticle">
         <h2 className="redH2">Superficially acquired skills:</h2>
         <div>
+          <span>Node.js</span>
+          <i className="fa-solid fa-seedling"></i>
+          <span>Mongo DB</span>
+          <i className="fa-solid fa-seedling"></i>
           <span>Python</span>
-          <i className="fa-solid fa-seedling"></i>
-          <span>Ansible</span>
-          <i className="fa-solid fa-seedling"></i>
-          <span>Terraform</span>
           <i className="fa-solid fa-seedling"></i>
           <span>Docker</span>
           <i className="fa-solid fa-seedling"></i>
           <span>Linux</span>
           <i className="fa-solid fa-seedling"></i>
           <span>PowerShell</span>
+          <i className="fa-solid fa-seedling"></i>
         </div>
       </article>
       <article className="fourthArticle">
         <h2 className="redH2">Skills in progress:</h2>
         <div>
-          <span>React Native</span>
-          <i className="fa-solid fa-seedling"></i>
           <span>Next.js 13</span>
           <i className="fa-solid fa-seedling"></i>
-          <span>Node.js</span>
+          <span>Ansible</span>
           <i className="fa-solid fa-seedling"></i>
-          <span>Mongo DB</span>
         </div>
       </article>
     </div>
