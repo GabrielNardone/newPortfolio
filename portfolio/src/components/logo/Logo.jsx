6fa01629d71781e81188d433b543497e29d1ca11
gabrielNardone <gabriel.f.nardone@gmail.com>
2023-10-19 19:55:32 -0300

    changes

--- portfolio/src/components/logo/Logo.jsx
+++ portfolio/src/components/logo/Logo.jsx
@@ -3,7 +3,6 @@ import "./logo.css"
 export const Logo = () => {
   return (
     <div className="logoDiv">
-        <img src="./computadora.png" alt="icon"/>
         <span className="hacked">GN</span>
         <p>Gabriel Nardone</p>
     </div>
