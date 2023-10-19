8a4f454d900579fae675f5e850badd0fc1a31fe3
gabrielNardone <gabriel.f.nardone@gmail.com>
2023-10-19 20:07:56 -0300

    changes

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -9,10 +9,6 @@ export const Header = ({ handleNavBarWidth }) => {
                 <p>Gabriel Nardone</p>
             </div>
             <h1>SUCCESS IS DIGITAL!</h1>
-            <h2 className="min-width-title">
-                <span>SUCCESS</span>
-                <span>IS DIGITAL!</span>
-            </h2>
             <button onClick={handleNavBarWidth}><i className="fa-solid fa-bars"></i></button>
         </div>
     )
